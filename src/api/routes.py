c9342760873b4319ead501957838ff17a39b0193
Nav-03 <navil.gabrie@gmail.com>
2022-02-16 00:21:23 +0000

    issue on fetch

--- src/api/routes.py
+++ src/api/routes.py
@@ -9,14 +9,14 @@ from flask_jwt_extended import jwt_required, get_jwt_identity, create_access_tok
 api = Blueprint('api', __name__)
 
 
-@api.route('/hello', methods=['POST', 'GET'])
-def handle_hello():
+# @api.route('/hello', methods=['POST', 'GET'])
+# def handle_hello():
 
-    response_body = {
-        "message": "Hello! I'm a message that came from the backend, check the network tab on the google inspector and you will see the GET request"
-    }
+#     response_body = {
+#         "message": "Hello! I'm a message that came from the backend, check the network tab on the google inspector and you will see the GET request"
+#     }
 
-    return jsonify(response_body), 200
+#     return jsonify(response_body), 200
 
 @api.route('/user', methods=['POST'])
 def create_user():
@@ -24,6 +24,14 @@ def create_user():
     db.session.add(user)
     db.session.commit()
     return jsonify(user.serialize())
+
+
+@api.route('/planet', methods=['GET'])
+def get_planet():
+    planet_query = Planet.query.all()
+    all_serialized_planets = list(map(lambda item:item.serialize(), planet_query))
+    return jsonify(all_serialized_planets)
+
 
 
 @api.route('/planet', methods=['POST'])
@@ -36,7 +44,26 @@ def create_planet():
     terrain = request.json.get('terrain', None)
     population = request.json.get('population', None)
     img_url = request.json.get('img_url', None)
-    
+
+
+@api.route('/character', methods=['GET'])
+def get_character():
+    character_query = Character.query.all()
+    all_serialized_characters = list(map(lambda item:item.serialize(), character_query))
+    return jsonify(all_serialized_characters)
+
+
+
+@api.route('/favorite', methods=['GET'])
+@jwt_required()
+def get_favorite():
+    current_user_id = get_jwt_identity()
+    user = User.query.get(current_user_id)
+    if user is None:
+        return jsonify({"msg": "User Not Found"}), 403
+    favorite_query = Favorite.query.filter_by(user_id=current_user_id)
+    all_serialized_favorite = list(map(lambda item:item.serialize(), favorite_query))
+    return jsonify(all_serialized_favorite)    
     planet = Planet(name=name,
                     climate=climate,
                     rotation_period=rotation_period,
@@ -50,28 +77,7 @@ def create_planet():
     return jsonify(planet.serialize())
 
 
-@api.route('/character', methods=['GET'])
-def get_character():
-    character_query = Character.query.all()
-    all_serialized_characters = list(map(lambda item:item.serialize(), character_query))
-    return jsonify(all_serialized_characters)
 
-@api.route('/planet', methods=['GET'])
-def get_planet():
-    planet_query = Planet.query.all()
-    all_serialized_planets = list(map(lambda item:item.serialize(), planet_query))
-    return jsonify(all_serialized_planets)
-
-@api.route('/favorite', methods=['GET'])
-@jwt_required()
-def get_favorite():
-    current_user_id = get_jwt_identity()
-    user = User.query.get(current_user_id)
-    if user is None:
-        return jsonify({"msg": "User Not Found"}), 403
-    favorite_query = Favorite.query.filter_by(user_id=current_user_id)
-    all_serialized_favorite = list(map(lambda item:item.serialize(), favorite_query))
-    return jsonify(all_serialized_favorite)
 
 @api.route('/token', methods=['POST'])
 def create_token():
